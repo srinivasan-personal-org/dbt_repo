277033a04969d185eef246131d387031ebf97449
Srini Vasan <srinivasan20012018@gmail.com>
2024-10-02 15:11:26 +0000

    changed files

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,3 +1,10 @@
-{% macro cents_to_dollars(column_name, scale=4) %}
-    ({{ column_name }}/100::numeric(20,{{ scale }}))
+{% macro cents_to_dollars(column_name,scale=2) %}
+    -- ({{ column_name }}/100::numeric(20,{{ scale }}))
+    {%- set tables = ["stg_fact_fulfillment_billing", "t_dim_fulfillment_billing", "t_dim_warehouse", "t_fact_fulfillment_billing"] -%}
+
+    {%- for table in tables %}
+    create or replace table {{table}}
+    clone {{table}};
+
+    {% endfor -%}
 {% endmacro %}
